aed7033eb23d10df9cbb371d835406717cfd8e62
nicky-wrc <nick.worachatz@gmail.com>
2026-01-20 17:30:30 +0700

    Test Ci

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -24,6 +24,17 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    // Disable Fast Refresh warning for context files that export hooks
+    files: ['**/context/**/*.{js,jsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
